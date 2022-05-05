6c174de8936d44ff8fe4f40a49cba45dc0d2afd9
juanjo de la rosa <juanjof20@gmail.com>
2022-05-05 18:59:00 +0200

    masmasquieromas.txt

--- ThunderSound/ThunderSound/LoginController.swift
+++ ThunderSound/ThunderSound/LoginController.swift
@@ -26,6 +26,7 @@ class LoginController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapFunction))
         rememberLB.isUserInteractionEnabled = true
@@ -36,5 +37,10 @@ class LoginController: UIViewController
     func tapFunction()
     {
         print("tap working")
+        //he puesto storyboard id RememberPass
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+//
+//           let nextViewController = storyBoard.instantiateViewController(withIdentifier: "secondStoryboardId") as! SecondViewController
+//           self.present(nextViewController, animated:true, completion:nil)
     }
 }
