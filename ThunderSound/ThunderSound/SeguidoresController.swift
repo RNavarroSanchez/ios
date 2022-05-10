81f43b7b8d204b25b3ac2930a54747855dba7524
juanjo de la rosa <juanjof20@gmail.com>
2022-05-10 19:03:48 +0200

    navigationFollowers

---
+++ ThunderSound/ThunderSound/SeguidoresController.swift
@@ -0,0 +1,17 @@
+//
+//  SeguidoresController.swift
+//  ThunderSound
+//
+//  Created by Juanjo
+//
+
+import UIKit
+
+class SeguidoresController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
---
+++ ThunderSound/ThunderSound/SeguidosController.swift
@@ -0,0 +1,17 @@
+//
+//  SeguidosController.swift
+//  ThunderSound
+//
+//  Created by Juanjo
+//
+
+import UIKit
+
+class SeguidosController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
