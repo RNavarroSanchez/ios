2ab5a3c6d2147774079371fea329f75448babed2
juanjo de la rosa <juanjof20@gmail.com>
2022-05-19 19:09:46 +0200

    pruebas PeticionLogin y register
    error con la transformación de data

--- ThunderSound/ThunderSound/LoginController.swift
+++ ThunderSound/ThunderSound/LoginController.swift
@@ -9,36 +9,62 @@ import UIKit
 
 class LoginController: UIViewController
 {
+    var myResponse: [String: Any] = [:]
     @IBOutlet var userTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var rememberLB: UILabel!
-    @IBAction func registerBT(_ sender: Any)
+    @IBAction func registerBT(_ sender: Any)//Registro
     {
-        //print("tapRegister working")
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "Registerid") as! RegisterController
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true, completion: nil)
     }
-    @IBAction func loginBT(_ sender: Any)
+    @IBAction func loginBT(_ sender: Any)//Inicio de Sesión
     {
-        //print("tapLogin working")
-        peticionLogin()
+        if userTF.text != "" || passwordTF.text != ""
+        {
+            peticionLogin(userTF: userTF.text!, passwordTF: passwordTF.text!)
+        } else
+        {
+            let alert = UIAlertController(title: "Error", message: "No puedes dejar un campo sin rellenar", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        var preferences = UserDefaults.standard
 
+        if let user = preferences.string(forKey: "userTF") //aqui va esto o el nick??
+        {
+            if let pass = preferences.string(forKey: "passwordTF")
+            {
+                if user != "" && pass != ""
+                {
+                    peticionLogin(userTF: user, passwordTF: pass)
+                }
+            }
+        }
         //tapOlvidar la contraseña
         let tapOlvidar = UITapGestureRecognizer(target: self, action: #selector(self.tapRemember))
         rememberLB.isUserInteractionEnabled = true
         rememberLB.addGestureRecognizer(tapOlvidar)
-
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureHandler))
+                view.addGestureRecognizer(tapGesture)
+    }
+    @objc func tapGestureHandler()
+    {
+        userTF.endEditing(true)
+        passwordTF.endEditing(true)
     }
 
     @objc
-    func tapRemember()
+    func tapRemember()//OLVIDAR CONTRASEÑA
     {
         //print("tapRemember working")
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -47,28 +73,15 @@ class LoginController: UIViewController
         self.present(vc, animated: true, completion: nil)
     }
     
-    func peticionLogin()
+    func peticionLogin(userTF: String, passwordTF: String)//Peticion Inicio de Sesión
     {
-        let Url = String(format: "http://127.0.0.1:8001/api/auth/login")
+        let Url = String(format: "http://127.0.0.1:8889/api/auth/login")//cambiar el puerto para pruebas
         guard let serviceUrl = URL(string: Url) else { return }
         var request = URLRequest(url: serviceUrl)
         request.httpMethod = "POST"
-        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-        let parameters: [String: Any] =
-        [
-            "request":
-            [
-                "nick": userTF.text,
-                "password": passwordTF.text
-            ]
-        ]
-        guard let httpBody =
-        try? JSONSerialization.data(withJSONObject: parameters, options: [])
-        else
-        {
-            return print("Error peticion")
-        }
-        request.httpBody = httpBody
+        request.setValue("Application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        let bodyData = "nick=\(userTF)&password=\(passwordTF)"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8);
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
             if let response = response
@@ -80,14 +93,57 @@ class LoginController: UIViewController
                 do
                 {
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
-//                    DispatchQueue.main.async
-//                    {
-//                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//                        let vc = storyboard.instantiateViewController(withIdentifier: "Inicioid") as! InicioController
-//                        vc.modalPresentationStyle = .fullScreen
-//                        self.present(vc, animated: true, completion: nil)
-//                    }
+                    DispatchQueue.main.async
+                    { [self] in
+                        print(json)
+                        self.myResponse = json as! [String: Any]
+                        if self.myResponse["code"] as! Int != 200
+                        {
+                            let alert = UIAlertController(title: "Error", message: myResponse["message"] as? String, preferredStyle: .alert)
+                            let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+                            alert.addAction(action)
+                            present(alert, animated: true)
+                            
+                        }
+//                        if self.myResponse["code"] as! Int == 404
+//                        {
+//                            let alert = UIAlertController(title: "Error", message: myResponse["message"] as? String, preferredStyle: .alert)
+//                            let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+//                            alert.addAction(action)
+//                            present(alert, animated: true)
+//                        }
+//                        if self.myResponse["code"] as! Int == 400
+//                        {
+//                            let errores = myResponse["errors"] as! [String: Any]
+//                            if let email = errores["email"] as? [String]
+//                            {
+//                                let alert = UIAlertController(title: "Error", message: email[0], preferredStyle: .alert)
+//                                let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+//                                alert.addAction(action)
+//                                present(alert, animated: true)
+//                            }
+//                            if let password = errores["password"] as? [String]
+//                            {
+//                                let alert = UIAlertController(title: "Error", message: password[0], preferredStyle: .alert)
+//                                let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+//                                alert.addAction(action)
+//                                present(alert, animated: true)
+//                            }
+//                        }
+                        if self.myResponse["code"] as! Int == 200
+                        {
+                            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                            let vc = storyboard.instantiateViewController(withIdentifier: "Inicioid") as! InicioController
+                            vc.modalPresentationStyle = .fullScreen
+                            self.present(vc, animated: true, completion: nil)
+                            
+                            let shared = UserDefaults.standard
+                            shared.setValue(userTF, forKey: "userTF")
+                            shared.setValue(passwordTF, forKey: "passwordTF")
+                            let datos: [String: Any] = myResponse["data"] as! [String: Any]
+                            shared.setValue(datos["token"] as! String, forKey: "token")
+                        }
+                    }
                 } catch
                 {
                     print(error)
--- ThunderSound/ThunderSound/RegisterController.swift
+++ ThunderSound/ThunderSound/RegisterController.swift
@@ -7,27 +7,72 @@
 
 import UIKit
 
-class RegisterController: UIViewController
+class RegisterController: UIViewController, UITextFieldDelegate
 {
     
     @IBAction func atrasBT(_ sender: Any) {dismiss(animated: true, completion: nil)}
     @IBOutlet var emailTFr: UITextField!
     @IBOutlet var passTFr: UITextField!
     @IBOutlet var passx2TFr: UITextField!
-    
     @IBOutlet var editarIMG: UIImageView!
     @IBOutlet var userTFr: UITextField!
     @IBOutlet var nameTFr: UITextField!
     @IBOutlet var subnameTFr: UITextField!
     @IBOutlet var descripcionTFr: UITextField!
+    var myDictionary: [String: Any] = [:]
     
     @IBAction func registerBTr(_ sender: Any)
     {
-        //print("tapRegister2 working")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "Inicioid") as! InicioController
-        vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated: true, completion: nil)
+        if emailTFr.text != "" && passTFr.text == passx2TFr.text && userTFr.text != "" && nameTFr.text != "" && subnameTFr.text != ""
+        {
+            let Url = String(format: "http://127.0.0.1:8889/api/usuarios")
+            guard let serviceUrl = URL(string: Url) else { return }
+            var request = URLRequest(url: serviceUrl)
+            request.httpMethod = "POST"
+            request.setValue("Application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            let bodyData = "correo=\(emailTFr.text ?? "")&password=\(passTFr.text ?? "")&nick=\(userTFr.text ?? "")&nombre=\(nameTFr.text ?? "")&apellidos=\(subnameTFr.text ?? "")&descripcion=\(descripcionTFr.text ?? "")"
+            request.httpBody = bodyData.data(using: String.Encoding.utf8);
+
+            let session = URLSession.shared
+            session.dataTask(with: request) { (data, response, error) in
+                if let response = response
+                {
+                    print(response)
+                }
+                if let data = data
+                {
+                    do
+                    {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        
+                        DispatchQueue.main.async
+                        {
+                            self.myDictionary = json as! [String: Any]
+
+                            if self.myDictionary["code"] as! Int == 200
+                            {
+                                let alert = UIAlertController(title: "LLego el momento...", message: "Registro completado con éxito", preferredStyle: .alert)
+                                let action = UIAlertAction(title: "Aceptar", style: .default, handler: {_ in
+                                    self.navigationController?.popToRootViewController(animated: true)
+                                })
+                                alert.addAction(action)
+                                self.present(alert, animated: true, completion: nil)
+                                
+                            } else
+                            {
+                                let alert = UIAlertController(title: "Error", message: self.myDictionary["message"] as? String, preferredStyle: .alert)
+                                let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+                                alert.addAction(action)
+                                self.present(alert, animated: true, completion: nil)
+                            }
+                        }
+                    } catch
+                    {
+                        print(error)
+                    }
+                }
+            }.resume()
+        } else {print("Error en la peticionRegister ")}
     }
     
     override func viewDidLoad()
@@ -41,5 +86,18 @@ class RegisterController: UIViewController
         // Redondear TextField
         self.descripcionTFr.layer.cornerRadius = 10
         self.descripcionTFr.clipsToBounds = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureHandler))
+                view.addGestureRecognizer(tapGesture)
+    }
+    @objc func tapGestureHandler()
+    {
+        emailTFr.endEditing(true)
+        passTFr.endEditing(true)
+        passx2TFr.endEditing(true)
+        userTFr.endEditing(true)
+        nameTFr.endEditing(true)
+        subnameTFr.endEditing(true)
+        descripcionTFr.endEditing(true)
     }
 }
